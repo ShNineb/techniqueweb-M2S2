4d50b982801a618e1aafcb2a269332081b80792f
Mei GAN <gan.mei@outlook.com>
2021-02-23 18:03:25 +0100

    update

--- project/models.py
+++ project/models.py
@@ -14,9 +14,26 @@ class User(UserMixin,db.Model):
 
 # le tableux geonames 
 class Geonames(db.Model):
-	geoid = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(1000))
-	langtitude = db.Column(db.Float)
+    geonameid=db.Column(db.Integer, primary_key=True, unique=True)
+    name=db.Column(db.String)
+    asciiname=db.Column(db.String)
+    alternatenames=db.Column(db.String)
+    latitude=db.Column(db.Float)
+    longitude=db.Column(db.Float)
+    feature_class=db.Column(db.String)
+    feature_code=db.Column(db.String)
+    country_code=db.Column(db.String)
+    cc2=db.Column(db.Char(2))
+    admin1_code=db.Column(db.Integer)
+    admin2_code=db.Column(db.Integer)
+    admin3_code=db.Column(db.Integer)
+    admin4_code=db.Column(db.Integer)
+    population=db.Column(db.Integer)
+    elevation=db.Column(db.Integer)
+    dem=db.Column(db.Integer)
+    timezone=db.Column(db.String)
+    modification=db.Column(db.String)
+    modified_by=db.Column(db.String)
 
 
 
