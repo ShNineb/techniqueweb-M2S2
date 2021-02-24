ca420d4b40b391517a6d4cc8b1fef27861ed1f89
Mei GAN <gan.mei@outlook.com>
2021-02-24 11:54:45 +0100

    version 0.11

---
+++ project_v0.11/resources/index.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, render_template,request
+from . import db
+from flask_login import login_required, current_user
+from .models import User, Geonames
+
+index = Blueprint('index', __name__)
+
+@index.route('/')
+def index_page():
+	# if current_user.name:
+	# 	return render_template('index.html',name=current_user.name)
+	return render_template('index.html')
+
+@index.route('/management', methods=['POST','GET'])
+@login_required
+def management():
+	if request.method == 'POST':
+		geoid = request.form['geoid']
+		id_search = Geonames.query.filter_by(geonameid=geoid).first()
+        # if id doesn't exist, we add a new one
+		if not id_search:
+			print(f"not found {id_search}")
+			return render_template('add.html', name=current_user.name )
+        #  if id exist in the database, user can modify or delete it
+		else:
+			print(f"found {id_search}")
+			return render_template('modify_delete.html', name=current_user.name, geonameid=id_search.geonameid)
+	else:
+		return render_template('management.html', name=current_user.name)
+
+
+@index.route('/add')
+@login_required
+def add():
+    return render_template('add.html', name=current_user.name)
+
+
---
+++ project_v0.11/resources/models.py
@@ -0,0 +1,40 @@
+from . import db
+from flask_login import UserMixin
+
+
+# le tableux user 
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    username = db.Column(db.String(200), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+    def __repr__(self):
+    	return '<Name %r>' % self.id
+
+# le tableux geonames 
+class Geonames(db.Model):
+    geonameid=db.Column(db.Integer, primary_key=True, unique=True)
+    name=db.Column(db.String)
+    asciiname=db.Column(db.String)
+    alternatenames=db.Column(db.String)
+    latitude=db.Column(db.Float)
+    longitude=db.Column(db.Float)
+    feature_class=db.Column(db.String)
+    feature_code=db.Column(db.String)
+    country_code=db.Column(db.String)
+    cc2=db.Column(db.String)
+    admin1_code=db.Column(db.Integer)
+    admin2_code=db.Column(db.Integer)
+    admin3_code=db.Column(db.Integer)
+    admin4_code=db.Column(db.Integer)
+    population=db.Column(db.Integer)
+    elevation=db.Column(db.Integer)
+    dem=db.Column(db.Integer)
+    timezone=db.Column(db.String)
+    modification=db.Column(db.Date)
+    modified_by=db.Column(db.String)
+
+
+
+    	
